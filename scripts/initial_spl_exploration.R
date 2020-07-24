17d1f9360e1a44ce7cd1ca2055070c4d4f5f8fa4
skraftarcher <sarcher@lumcon.edu>
2020-07-23 20:38:27 -0500

    cleaned up code and commented

--- scripts/initial_spl_exploration.R
+++ scripts/initial_spl_exploration.R
@@ -16,70 +16,43 @@ out19<-read_xlsx("odata/Broadband SPL RCA.xlsx",sheet = "RCA_Out_2019")
 out20<-read_xlsx("odata/Broadband SPL RCA.xlsx",sheet = "RCA_Out_2020")
 
 #data manipulation----
+# add in identifiers that allow us to combine the datasets
 in192<-in19 %>%
-  separate(Time,into=c("hr","min","sec"),sep=":")%>%
-  separate(hr,into=c("wste","hr"),sep=" ",remove=TRUE)%>%
-  select(-wste)%>%
-  group_by(Month,Day,hr)%>%
-  summarize(spl50=median(SPL),
-            rmsspl=sqrt(mean(SPL^2)),
-            spl01=quantile(SPL,.01),
-            spl05=quantile(SPL,.05),
-            spl95=quantile(SPL,.95),
-            spl99=quantile(SPL,.99))%>%
-  mutate(mdh=paste(Month,"-",Day,"-",hr),
-         grp="RCA In 2019",
+  mutate(grp="RCA In 2019",
          rca="in",
          year=2019)
 
 out192<-out19 %>%
-  separate(Time,into=c("hr","min","sec"),sep=":")%>%
-  separate(hr,into=c("wste","hr"),sep=" ",remove=TRUE)%>%
-  select(-wste)%>%
-  group_by(Month,Day,hr)%>%
-  summarize(spl50=median(SPL),
-            rmsspl=sqrt(mean(SPL^2)),
-            spl01=quantile(SPL,.01),
-            spl05=quantile(SPL,.05),
-            spl95=quantile(SPL,.95),
-            spl99=quantile(SPL,.99))%>%
-  mutate(mdh=paste(Month,"-",Day,"-",hr),
-         grp="RCA Out 2019",
+  mutate(grp="RCA Out 2019",
          rca="out",
          year=2019)
 
 in202<-in20 %>%
-  separate(Time,into=c("hr","min","sec"),sep=":")%>%
-  separate(hr,into=c("wste","hr"),sep=" ",remove=TRUE)%>%
-  select(-wste)%>%
-  group_by(Month,Day,hr)%>%
-  summarize(spl50=median(SPL),
-            rmsspl=sqrt(mean(SPL^2)),
-            spl01=quantile(SPL,.01),
-            spl05=quantile(SPL,.05),
-            spl95=quantile(SPL,.95),
-            spl99=quantile(SPL,.99))%>%
-  mutate(mdh=paste(Month,"-",Day,"-",hr),
-         grp="RCA In 2020",
+  mutate(grp="RCA In 2020",
          rca="in",
          year=2020)
 
 out202<-out20 %>%
+  mutate(grp="RCA Out 2020",
+         rca="out",
+         year=2020)
+
+# create a dataset that is aggregated by the hour, 
+# several variables are calculated, 1,5,50,95, and 99 quantiles as well as the root mean square error
+
+
+splhr<-bind_rows(in192,in202,out192,out202)%>%
   separate(Time,into=c("hr","min","sec"),sep=":")%>%
   separate(hr,into=c("wste","hr"),sep=" ",remove=TRUE)%>%
   select(-wste)%>%
-  group_by(Month,Day,hr)%>%
+  group_by(year,rca,grp,Month,Day,hr)%>%
   summarize(spl50=median(SPL),
             rmsspl=sqrt(mean(SPL^2)),
             spl01=quantile(SPL,.01),
             spl05=quantile(SPL,.05),
             spl95=quantile(SPL,.95),
             spl99=quantile(SPL,.99))%>%
-  mutate(mdh=paste(Month,"-",Day,"-",hr),
-         grp="RCA Out 2020",
-         rca="out",
-         year=2020)
-splhr<-bind_rows(in192,in202,out192,out202)
+  mutate(mdh=paste(Month,"-",Day,"-",hr))
 write.csv(splhr,"wdata/splbyhour.csv")
 
 # grouped by hour----
@@ -87,72 +60,62 @@ ggplot(splhr)+
   geom_line(aes(y=rmsspl,x=mdh,color=as.factor(year),group=grp))+
   facet_wrap(~rca)
 
-#data manipulation2----
-in19d<-in19 %>%
-  group_by(Month,Day)%>%
+# create a dataset that is aggregated by day, same variables are calculated 
+
+
+spld<-bind_rows(in192,in202,out192,out202)%>%
+  separate(Time,into=c("hr","min","sec"),sep=":")%>%
+  separate(hr,into=c("wste","hr"),sep=" ",remove=TRUE)%>%
+  select(-wste)%>%
+  group_by(year,rca,grp,Month,Day)%>%
   summarize(spl50=median(SPL),
             rmsspl=sqrt(mean(SPL^2)),
             spl01=quantile(SPL,.01),
             spl05=quantile(SPL,.05),
             spl95=quantile(SPL,.95),
             spl99=quantile(SPL,.99))%>%
-  pivot_longer(spl50:spl99,names_to = "measure",values_to = "spd")%>%
-  mutate(mdh=paste(Month,"-",Day),
-         grp="RCA In 2019",
-         rca="in",
-         year=2019)
-
-out19d<-out19 %>%
-  group_by(Month,Day)%>%
-  summarize(spl50=median(SPL),
-            rmsspl=sqrt(mean(SPL^2)),
-            spl01=quantile(SPL,.01),
-            spl05=quantile(SPL,.05),
-            spl95=quantile(SPL,.95),
-            spl99=quantile(SPL,.99))%>%
-  pivot_longer(spl50:spl99,names_to = "measure",values_to = "spd")%>%
-  mutate(mdh=paste(Month,"-",Day),
-         grp="RCA Out 2019",
-         rca="out",
-         year=2019)
-
-in20d<-in20 %>%
-  group_by(Month,Day)%>%
-  summarize(spl50=median(SPL),
-            rmsspl=sqrt(mean(SPL^2)),
-            spl01=quantile(SPL,.01),
-            spl05=quantile(SPL,.05),
-            spl95=quantile(SPL,.95),
-            spl99=quantile(SPL,.99))%>%
-  pivot_longer(spl50:spl99,names_to = "measure",values_to = "spd")%>%
-  mutate(mdh=paste(Month,"-",Day),
-         grp="RCA In 2020",
-         rca="in",
-         year=2020)
-
-out20d<-out20 %>%
-  group_by(Month,Day)%>%
-  summarize(spl50=median(SPL),
-            rmsspl=sqrt(mean(SPL^2)),
-            spl01=quantile(SPL,.01),
-            spl05=quantile(SPL,.05),
-            spl95=quantile(SPL,.95),
-            spl99=quantile(SPL,.99))%>%
-  pivot_longer(spl50:spl99,names_to = "measure",values_to = "spd")%>%
-  mutate(mdh=paste(Month,"-",Day),
-         grp="RCA Out 2020",
-         rca="out",
-         year=2020)
-
-d20<-in20d%>%
-  ungroup()%>%
-  select(mdh)%>%
-  distinct()
-spld<-bind_rows(in19d,in20d,out19d,out20d)%>%
-  filter(mdh %in% d20$mdh)
+  mutate(mdh=paste(Month,"-",Day))
 
 write.csv(bind_rows(in19d,in20d,out19d,out20d),"wdata/splbyday.csv")
+
 # grouped by day----
 ggplot(spld)+
-  geom_line(aes(y=spd,x=mdh,color=as.factor(year),group=grp))+
-  facet_grid(measure~rca,scales="free")
+  geom_line(aes(y=spl50,x=mdh,color=as.factor(year),group=grp))+
+  facet_grid(~rca,scales="free")
+
+# subset down to only time periods recorded in 2020
+# create datasets that only include the time periods where there is data in 2020 then subset the
+# larger datasets down to those time periods
+
+d20<-spld%>%
+  ungroup()%>%
+  filter(year==2020)%>%
+  select(mdh)%>%
+  distinct()
+
+
+hr20<-splhr%>%
+  ungroup()%>%
+  filter(year==2020)%>%
+  select(mdh)%>%
+  distinct()
+
+spld2<-spld%>%
+  filter(mdh %in% d20$mdh)
+
+splhr2<-splhr%>%
+  filter(mdh %in% hr20$mdh)
+
+
+# plotting only data in both data sets
+
+ggplot(spld2)+
+  geom_line(aes(y=spl50,x=mdh,color=as.factor(year),group=grp))+
+  facet_grid(~rca,scales="free")
+
+
+ggplot(splhr2)+
+  geom_line(aes(y=spl50,x=mdh,color=as.factor(year),group=grp))+
+  facet_grid(~rca,scales="free")
+
+
