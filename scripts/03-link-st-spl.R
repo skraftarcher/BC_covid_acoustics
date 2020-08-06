4117189f1a3777807eecc38421205e8d13b9dc83
skraftarcher <sarcher@lumcon.edu>
2020-08-06 15:02:11 -0500

    streamlined and made linking spl to soundtrap file much much faster.

--- scripts/03-link-st-spl.R
+++ scripts/03-link-st-spl.R
@@ -19,27 +19,21 @@ r19in2<-tibble(stfile=r19in) %>%
   separate(stfile,into=c("st","y","m","d","h","min","s","e"),
            sep=c(11,13,15,17,19,21,23),
            remove=FALSE)%>%
-  select(-st,-e)%>%
   mutate(y=2019,
          site="RCA_In",
-         DateTime=ymd_hms(paste(y,m,d,h,min,s)),
-         etime=lead(DateTime,default=ymd_hms(20190625054114))-seconds(1),# the default statement here adds in the 
-         # time 30 minutes after the last file name. if the recording interval is shorter for 2020
-         # this should be adjusted. Also all attempts to extract that value automatically
-         # produced errors for me so I had to enter it mannually.
-         tint=interval(start=DateTime,end=etime))
- #        tint=interval(start=DateTime,end=etime))
+         dt=ymd_hms(paste(y,m,d,h,min,s)),
+         ID=row_number())%>%
+  select(stfile,dt,site,ID)
+         
 # bring in minute by minute SPL data
 in19 <- read_xlsx("odata/Broadband SPL RCA.xlsx", sheet = "RCA_In_2019")
 # make DateTime have the correct year
 year(in19$DateTime)<-2019
 # link spl and soundtrap file
-# note this is slow and I am 100% sure there is a better way to do it but this is 
-# what I got for now and it works...eventually
-in19$stfile<-NA
-for(i in 1:nrow(in19)){
-  in19$stfile[i]<-r19in2[in19$DateTime[i] %within% r19in2$tint,1]
-}
+in19<-cbind(in19,ID=findInterval(in19$DateTime,r19in2$dt))%>%
+  left_join(r19in2)%>%
+  select(-ID,-dt)
 
 
-                
+             
+  
