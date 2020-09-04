25cb3a6f7071f878f16f028da1db2902460763a5
skraftarcher <sarcher@lumcon.edu>
2020-09-04 11:25:56 -0500

    manually identify periods to analyze

--- scripts/05_boat_passage.R
+++ scripts/05_boat_passage.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(lubridate)
 library(readxl)
 
+
 splm<-read_rds("wdata/spl_by_min.rds")%>%
   mutate(hr=hour(Time),
          mn=minute(Time),
@@ -243,7 +244,7 @@ year(spl$DateTime)<-spl$Year #year in the date is messed up, fix that
 
 mv<-bind_rows(mv19,mv20)%>%
    arrange (DateTime)%>% # make sure dataset goes from earliest to latest
-   mutate(st=DateTime-minutes(30),et=DateTime+minutes(30))%>% # create the window to match to
+   mutate(st=DateTime-minutes(60),et=DateTime+minutes(60))%>% # create the window to match to
    pivot_longer(st:et,names_to="tt",values_to="tint")%>% # turn it in to a long data format so we 
    mutate(inter=row_number()) # give each interval a number- using the find interval it assigns ID based on the row # of the first line in the
 # interval i.e., the interval between rows 1 and 2 is interval 1
@@ -292,3 +293,92 @@ for(i in 1:length(interlist)){
     scale_x_datetime(date_minor_breaks="5 mins")
   ggsave(paste0("manual_figures/fig_",p1$Year[i],p1$Month[i],p1$Day[i],".jpg"))
 }
+
+spl3<-spl%>%
+  filter(inter %in% mv2$inter)%>% # filter down only to intervals within 30 mins of the ferry passing (30 before or after)
+  left_join(mv2)%>% # join the ferry passage info
+  mutate(Hr=hour(DateTime),datehr=ymd_h(paste(Year,Month,Day,Hr)))%>% # create a date hr variable to link with wind
+  left_join(wthr)%>% # join in wind
+  group_by(inter)%>% # grouping by interval because some intervals span 2 hours
+  mutate(wsp2=mean(wspeed,na.rm = TRUE))%>% # create a mean wind value for the interval
+  filter(!is.na(wsp2) & wsp2 <20 & DateTime < "2020-05-05")%>%
+  ungroup()%>%
+  group_by(inter)%>%
+  mutate(tperiods=ifelse(DateTime<dt,"pre","post"))%>%
+  group_by(inter,tperiods)%>%
+  mutate(pre.pst=case_when(
+    tperiods=="pre"& zoo::rollmax(SPL,k=5,fill=NA,align="left")<100~1,
+    tperiods=="pre"& zoo::rollmax(SPL,k=5,fill=NA,align="left")>100~0,
+    tperiods=="pre"& is.na(zoo::rollmax(SPL,k=5,fill=NA,align="left"))~0,
+    tperiods=="post"~0),
+    post.pst=case_when(
+      tperiods=="post"& zoo::rollmax(SPL,k=5,fill=NA,align="right")<100~1,
+      tperiods=="post"& zoo::rollmax(SPL,k=5,fill=NA,align="right")>100~0,
+      tperiods=="post"& is.na(zoo::rollmax(SPL,k=5,fill=NA,align="right"))~0,
+      tperiods=="pre"~0),
+    pt.int=ifelse(pre.pst==1|post.pst==1,1,0),
+    ferry.int=interval(dt-minutes(6),dt-minutes(1)),
+    ferry.prd=if_else(DateTime %within% ferry.int,1,0))%>%
+  group_by(inter,tperiods,pt.int)%>%
+  mutate(pre.pst=ifelse(pre.pst==1& dt-DateTime==min(dt-DateTime),1,0),
+         post.pst=ifelse(post.pst==1& DateTime-dt==min(DateTime-dt),1,0))%>%
+  ungroup(tperiods,pt.int)%>%
+  mutate(keep.inter=ifelse(sum(pre.pst)!=0 & sum(post.pst)!=0,1,0))%>%
+  filter(keep.inter==1)%>%
+  group_by(inter,tperiods)
+
+spl3a<-spl3 %>% 
+  ungroup()%>%
+  filter(ferry.prd==1)%>%
+  group_by(inter)%>%
+  summarize(ferryspl=mean(SPL))%>%
+  filter(ferryspl>110)
+
+spl3<-filter(spl3,inter %in%spl3a$inter)
+  
+
+spl4<-spl3%>%
+  filter(pre.pst==1)%>%
+  ungroup()%>%
+  select(inter,pre.dt=DateTime)%>%
+  mutate(pre.int=interval(pre.dt,pre.dt+minutes(5)))
+spl5<-spl3%>%
+  filter(post.pst==1)%>%
+  ungroup()%>%
+  select(inter,post.dt=DateTime)%>%
+  mutate(post.int=interval(post.dt-minutes(5),post.dt))
+
+spl3<-spl3%>%
+  ungroup()%>%
+  left_join(spl4)%>%
+  left_join((spl5))%>%
+  mutate(pre.prd=if_else(DateTime %within% pre.int,1,0),
+         post.prd=if_else(DateTime %within% post.int,1,0),
+         prd=case_when(
+           pre.prd==1~"pre",
+           post.prd==1~"post",
+           ferry.prd==1~"ferry",
+           pre.prd==0 & post.prd==0 & ferry.prd ==0~"none"))
+
+
+
+  
+  
+
+
+
+interlist2<-unique(spl3$inter)
+for(i in 1:length(interlist2)){
+  p1<-spl3%>%
+    filter(inter==interlist2[i])
+    ggplot(data=p1)+
+    geom_line(aes(y=SPL,x=DateTime,group=inter))+
+    geom_line(aes(y=88,x=DateTime,group=inter,color=prd),size=1.5)+
+      scale_color_manual(values=c("red","white","red","red"),name="Potential interval")+
+    theme_bw()+
+    geom_vline(aes(xintercept=dt),color="red",linetype="dashed")+
+    scale_x_datetime(date_minor_breaks="5 mins")+
+      theme(legend.position="none")
+  ggsave(paste0("manual_figures/fig_",p1$Year[i],p1$Month[i],p1$Day[i],"_withperiods.jpg"))
+}
+
