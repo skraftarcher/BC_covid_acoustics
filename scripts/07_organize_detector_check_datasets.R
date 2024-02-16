6f0120e2817ba65ecab3ca687bf7ebf6408b5a4f
Stephanie Archer <sarcher@LUMCON.edu>
2024-02-16 16:27:11 -0600

    update to organize data for Xavier

--- scripts/07_organize_detector_check_datasets.R
+++ scripts/07_organize_detector_check_datasets.R
@@ -5,6 +5,7 @@ source("scripts/install_packages_function.R")
 lp(pck="tidyverse")
 lp(pck="lubridate")
 lp(pck="readxl")
+lp("Rraven")
 
 # load data
 # one minute chunks
@@ -13,7 +14,7 @@ am.check<-Rraven::imp_raven(path = "w.selection.tables/",
                             all.data = TRUE)%>%
   select(-selec.file)
 # five minute chunks
-check19<-Rraven::imp_raven(path = here::here("w.selection.tables"),
+check19<-Rraven::imp_raven(path = "w.selection.tables",
                            files = "boat_passage_random_selections_updated_Dec152020.txt",
                            all.data = TRUE)%>%
   select(-selec.file)
@@ -33,6 +34,7 @@ check20<-Rraven::imp_raven(path = "w.selection.tables/",
 
 all19<-read_rds("wdata/alldata2019.rds")
 all20<-read_rds("wdata/alldata2020.rds")
+
 
 # organize data
 
@@ -115,3 +117,90 @@ dcheck20<-left_join(man20,all20)%>%
 #write out these datasets
 write_rds(dcheck19,"wdata/detector_check_2019.rds")
 write_rds(dcheck20,"wdata/detector_check_2020.rds")
+
+# organize data to output for Xavier
+
+all192<-all19%>%
+  select(-fish.calls,-wind_spd,-wind_dir,-precip_amt,-tide,-wave.ht,
+         -wave.prd,-wave.ht2,-wave.prd2,-temp)
+
+all202<-all20%>%
+  select(-fish.calls,-wind_spd,-wind_dir,-precip_amt,-tide,-wave.ht,
+         -wave.prd,-wave.ht2,-wave.prd2,-temp)
+
+xav19<-check19%>%
+  select(-Class,-'Sound type',-'Software',-Confidence,-selec.file1,-Selection_X,
+         -updated.class,-updated.confidence,-Inter,-Period,-`Manual Type`)%>%
+  filter(`Manual Class`%in%c("NH","N","F","FS"))%>%
+  separate(`Begin File`,into=c("pre","dt","ext"),sep="\\.",remove = FALSE)%>%
+  separate(dt,into=c("yr","mnth","d","hr","mins","sec"),sep=c(2,4,6,8,10),convert = TRUE)%>%
+  mutate(yr=yr+2000)%>%
+  select(-sec,-pre,-ext)%>%
+  left_join(all192)
+
+xav20<-check20%>%
+  select(-man.type,-Class,-`Sound type`,-Software,-Confidence,-stfile,
+         -st,-s,-ext,-into.file,-datetime,-old.selection)%>%
+  mutate(man.class=case_when(
+    man.class=="F"~"F",
+    man.class=="N"~"N",
+    man.class=="B"~"N",
+    man.class=="K"~"F",
+    man.class=="FS"~"FS",
+    man.class=="NF"~"N"))%>%
+  rename(SPL.Int=spl.interval,`Manual Class`=man.class,mnth=m,mins=min,Comments=comments)%>%
+  left_join(all202)
+
+xav20b<-bind_rows(xav19[0,],xav20)
+
+# get the SPL Interval for each file and SPL
+spl.ints<-am.check[,colnames(am.check)%in%colnames(xav19)]%>%
+  separate(`Begin File`,into=c("st","dt","ext"),sep="\\.",remove=FALSE)%>%
+  separate(dt,into=c("yr","mnth","d","hr","mins","sec"),sep=c(2,4,6,8,10),convert=TRUE)%>%
+  mutate(yr=yr+2000,
+         dt=ymd_hms(paste(yr,mnth,d,hr,mins,sec)))%>%
+  select(`Begin File`,dt,Selection)
+
+spl.ints$spl.row<-findInterval(spl.ints$dt,all192$SPL.start.time)
+
+spl.ints$SPL.Int<-all192$SPL.Int[spl.ints$spl.row]
+
+spl.ints<-spl.ints[,c(-2,-4)]
+
+xav192<-am.check[,colnames(am.check)%in%colnames(xav19)]%>%
+  select(-SPL)%>%
+  left_join(spl.ints)%>%
+  left_join(all192)
+
+xav192b<-bind_rows(xav19[0,],xav192)
+
+xav193<-follow.check%>%
+  rename(SPL.Int=spl.interval,`Manual Class`=man.class,mnth=m,mins=min)
+
+xav193<-xav193[,colnames(xav193) %in% colnames(xav19)]%>%
+  select(-yr,-mnth,-d,-mins,-hr)%>%
+  left_join(all192)
+
+xav193b<-bind_rows(xav19[0,],xav193)
+
+xav2019<-bind_rows(xav19,xav192b,xav193b)%>%
+  arrange(`Begin File`,`File Offset (s)`)%>%
+  mutate(Selection=row.names(.))%>%
+  distinct()
+
+xav2020<-xav20b%>%
+  arrange(`Begin File`,`File Offset (s)`)%>%
+  mutate(Selection=row.names(.))%>%
+  distinct()
+
+write.table(xav2019,# the object you want to export as a selection table 
+            file = "selection.tables.xavier/all2019selections.txt",# the path and file name using the file extension .txt
+            sep = "\t", #how to delineate data
+            row.names = FALSE, #row names will mess things up
+            quote = FALSE)#putting things in quotes will mess things up.
+
+write.table(xav2020,# the object you want to export as a selection table 
+            file = "selection.tables.xavier/all2020selections.txt",# the path and file name using the file extension .txt
+            sep = "\t", #how to delineate data
+            row.names = FALSE, #row names will mess things up
+            quote = FALSE)#putting things in quotes will mess things up.
